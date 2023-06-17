cfeda2d1b03f8da58c145389d31b24ba6337208c
pepeien <erickfcaldeira@gmail.com>
2023-06-17 02:48:39 -0300

    Introduced ingredient selector component

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,8 @@ export { AutocompleteComponent, AutocompleteModule } from './autocomplete';
 export { AvatarComponent, AvatarModule } from './avatar';
 export { DialogComponent, DialogModule, DialogProps } from './dialog';
 export { FileSelectorComponent, FileSelectorModule } from './file-selector';
+export { IngredientCreatorComponent, IngredientCreatorModule } from './ingredient-creator';
+export { IngredientSelectorComponent, IngredientSelectorModule } from './ingredient-selector';
 export { LogoComponent, LogoModule } from './logo';
 export { MenuComponent, MenuModule } from './menu';
 export { NavbarComponent, NavbarModule } from './navbar';
